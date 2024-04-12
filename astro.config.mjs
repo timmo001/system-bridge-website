191a2b3ad696fdd2a8a51320804091a4ce57e7f2
Aidan Timson <aidan@timmo.dev>
2024-04-12 20:30:10 +0100

    Update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,6 @@ import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    viewTransitions: true,
-  },
   integrations: [robotsTxt(), sitemap(), tailwind()],
   site: "https://system-bridge.timmo.dev",
   markdown: {
