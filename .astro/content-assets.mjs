008a4fd743efc3ea44deff416981c1a03081df6d
Aidan Timson <aidan@timmo.dev>
2026-02-03 10:49:31 +0000

    Remove

--- .astro/content-assets.mjs
+++
@@ -1,4 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_jbkaP from "../../assets/logo.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
-export default new Map([["../../assets/logo.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_jbkaP]]);
-		
