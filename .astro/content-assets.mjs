e40af8c47d98273e3e4a843ffd9ac809ca4f09b8
Aidan Timson <aidan@timmo.dev>
2026-02-03 10:15:38 +0000

    Setup

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,4 +1,1 @@
-
-import __ASTRO_IMAGE_IMPORT_Z2gGaEW from "../../assets/houston.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
-export default new Map([["../../assets/houston.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_Z2gGaEW]]);
-		
+export default new Map();
